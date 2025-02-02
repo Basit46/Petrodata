627c04716748f1fdb0c37ecd689f9035ba98d0de
Basit46 <hassanbasitope@gmail.com>
2025-02-02 13:37:09 +0100

    update: Widget select

--- app/widgets/layout.jsx
+++ app/widgets/layout.jsx
@@ -3,6 +3,7 @@
 import React from "react";
 import WidgetSideBar from "../components/WidgetSideBar";
 import { useGlobalContext } from "../context/GlobalContext";
+import Link from "next/link";
 
 const layout = ({ children }) => {
   const { selectedWidgets } = useGlobalContext();
@@ -12,12 +13,21 @@ const layout = ({ children }) => {
       <WidgetSideBar />
       <div className="flex-1 h-full py-[30px]">{children}</div>
 
-      <div className="w-[20%] h-[400px] py-[30px] border-l border-l-gray">
-        {selectedWidgets.map((widget) => (
-          <p key={widget.id}>
-            {widget.id} - {widget.size}
-          </p>
-        ))}
+      <div className="w-[20%] h-[400px] py-[30px]">
+        <div>
+          {selectedWidgets.map((widget) => (
+            <p key={widget.id}>
+              Widget Id: {widget.id} - Size: {widget.size}
+            </p>
+          ))}
+        </div>
+
+        <Link
+          href="/dashboard"
+          className="mt-[20px] mx-auto w-[64px] h-[36px] grid place-items-center rounded-[36px] bg-[#525252] border border-[#737373]"
+        >
+          Done
+        </Link>
       </div>
     </div>
   );
